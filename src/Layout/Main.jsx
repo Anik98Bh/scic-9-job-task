7354c71b1bb9889baa038ee9753ae2b295feb849
Anik98B <anikbhowmik0188@gmail.com>
2024-07-16 03:26:20 +0600

    create a navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../pages/shared/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <div className="">
+                <Navbar></Navbar>
+            </div>
             <Outlet></Outlet>
         </div>
     );
