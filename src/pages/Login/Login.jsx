1d73e43449cb1082e25817b73746d159c1f082b3
Anik98B <anikbhowmik0188@gmail.com>
2024-07-16 18:07:10 +0600

    create login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,66 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const emailContact = form.email.value || form.contact?.value;
+        const password = form.password.value;
+        const newForm={emailContact,password};
+        console.log(newForm);
+        // signIn(email, password)
+        // .then((result) => {
+        //     const user = result.user;
+        //     // console.log(user)
+        //     Swal.fire({
+        //         position: "top-end",
+        //         icon: "success",
+        //         title: "User Login successfully",
+        //         showConfirmButton: false,
+        //         timer: 1500
+        //     });
+        //     navigate(from, { replace: true });
+        // })
+        //   .catch((error) => {
+        //     console.error(error)
+        //   });
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='bg-[#DC1195] p-2'>
+                <h1 className="text-5xl font-bold text-center text-white">Login now!</h1>
+            </div>
+            <div className="hero min-h-screen bg-[#DC1195]">
+                <div className="hero-content flex-col lg:flex-row">
+                <div className="w-1/2">
+                        <img className="rounded-xl animate-pulse w-96 h-96" src={'https://i.postimg.cc/xdhkkMdf/3d-hand-hold-smartphone-with-authentication-form.jpg'} alt="" />
+                    </div>
+                    <div className="card md:w-1/2 bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email/Mobile Number</span>
+                                </label>
+                                <input type={Number?"number":"email"} name="emailContact" placeholder="email or mobile number" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="password" className="input input-bordered" />
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn bg-[#DC1195] text-white" type="submit" value="Login" />
+                            </div>
+                        </form>
+                        <p className="text-center mt-2 mb-5">New to here? Please <Link className="text-[#DC1195] font-bold" to="/register">Register</Link></p>
+                        {/* <SocialLogin></SocialLogin> */}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const navOptions = <>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/login">Login</Link></li>
-    <li><Link to="/register">Register</Link></li>
-</>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
+    </>
     return (
         <div>
-            <div className="navbar bg-[#DC1195] text-white">
+            <div className="navbar bg-[#DC1195] text-white rounded-br-xl">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -31,11 +31,12 @@ const Navbar = () => {
                             {navOptions}
                         </ul>
                     </div>
-                   <div className="">
-                    <button className="btn btn-ghost text-xl">DigiWallet<img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/128/10017/10017650.png" alt="" /></button>
-                    
-                   {/* <a className="btn btn-ghost text-xl">DigiWallet</a> */}
-                   </div>
+                    <div className="">
+                        <Link to={"/"}>
+                            <button className="btn btn-ghost text-xl">DigiWallet<img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/128/10017/10017650.png" alt="" /></button>
+                        </Link>
+
+                    </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -43,7 +44,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn bg-[#DC1195] text-white border-none">Button</a>
+                    <a className="btn bg-[#DC1195] text-white border-none">Log Out</a>
                 </div>
             </div>
         </div>
