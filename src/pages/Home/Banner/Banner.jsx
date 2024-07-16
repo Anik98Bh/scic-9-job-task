cc3cc0c00b0493a355c5e1ced8092ed127fe8506
Anik98B <anikbhowmik0188@gmail.com>
2024-07-16 11:38:35 +0600

    added a banner section

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+
+const Banner = () => {
+    return (
+        <div className="flex gap-2">
+            <img className="w-1/2 bg-[#DC1195] rounded-b-xl" src={"https://cdn-icons-png.flaticon.com/512/10017/10017650.png"} alt="" />
+            <div className="pt-20 px-10">
+                <h1 className="text-4xl font-bold text-center">Ready to take payments using DigiWallet?</h1>
+                <p className="mt-4">Integrate with the payment solution that fits your business need
+                and Enrich your customers online payment experience.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
